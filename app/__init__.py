d51d51dea332d6effce1c3d2e90a5d5632c798fd
Nyota254 <knyota66@gmail.com>
2019-09-22 21:51:28 +0300

    connect the database with the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from config import configurations
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app(configname):
     '''
     Method for creating the app to enable for easier configurations
     '''
     app.config.from_object(configurations[configname])
+
+    #Importing BluePrints
     from .auth import auth as auth_blueprint
     from .main import main as main_blueprint
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
+    #Initialising the Importations
+    db.init_app(app)
+    bootstrap.init_app(app)
 
     return app
