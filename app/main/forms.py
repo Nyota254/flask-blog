905f39313ac773eeac222f289928c63cb3f878d6
Nyota254 <knyota66@gmail.com>
2019-09-23 10:34:36 +0300

    add the functionality to views.py file

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from . import main
+from flask_wtf import FlaskForm
+from wtforms import SubmitField,StringField,TextAreaField,SelectField
+from wtforms.validators import Required
+from ..models import Article,Comment
+
+class ArticleUploadForm(FlaskForm):
+    article = TextAreaField('Article',validators=[Required()])
+    category = SelectField('Category',choices=[('Interview','Interview'),('Pick-up','Pick-up'),('Product','Product'),('Promotion','Promotion')])
+    submit = SubmitField('Add Article')
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('comment on the post',validators=[Required()])
+    submit = SubmitField('Add Comment')
+
+class UpdateProfile(FlaskForm):
+    bio = StringField('About You',validators=[Required()])
+    submit = SubmitField('Add Bio')
